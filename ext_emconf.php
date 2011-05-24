7fde75a8b3de85f9aa52b20335beb1bf1189f6fd
nikola.stojiljkovic <nikola.stojiljkovic@e8cb78a8-64f2-4011-8042-bd1ea5058d47>
2011-05-24 07:03:02 +0000

    #26437 - picture gallery

---
+++ ext_emconf.php
@@ -0,0 +1,48 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "ed_extbase".
+#
+# Auto generated 23-05-2011 21:11
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Essential Dots Extbase add-on library',
+	'description' => '',
+	'category' => 'plugin',
+	'author' => 'Nikola Stojiljkovic',
+	'author_email' => 'nikola.stojiljkovic@essentialdots.com',
+	'shy' => '',
+	'dependencies' => 'extbase,fluid',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'beta',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.1.0',
+	'constraints' => array(
+		'depends' => array(
+			'extbase' => '',
+			'fluid' => '',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:5:{s:16:"ext_autoload.php";s:4:"130f";s:12:"ext_icon.gif";s:4:"b280";s:49:"Classes/ViewHelpers/Widget/PaginateViewHelper.php";s:4:"b8b5";s:60:"Classes/ViewHelpers/Widget/Controller/PaginateController.php";s:4:"d39b";s:66:"Resources/Private/Templates/ViewHelpers/Widget/Paginate/Index.html";s:4:"55a2";}',
+	'suggests' => array(
+	),
+);
+
+?>
